39bff0dcb9294e13a62a283b358e3db08a32844d
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2024-12-06 16:57:07 +0000

    apply formatter

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 console.log('loaded main.js');
-document.getElementById('email-clipboard').addEventListener('click', function() {
+document.getElementById('email-clipboard').addEventListener('click', function () {
     navigator.clipboard.writeText('ethan@hawksley.dev');
 });
-document.getElementById('email-clipboard').addEventListener('mousedown', function() {
+document.getElementById('email-clipboard').addEventListener('mousedown', function () {
     const svg = document.getElementById('copy-svg');
     svg.style.fill = '#8f9398';
 });
-window.addEventListener('mouseup', function() {
+window.addEventListener('mouseup', function () {
     const svg = document.getElementById('copy-svg');
     svg.style.fill = '#5f6368';
 });
