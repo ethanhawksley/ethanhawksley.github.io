3803b53a7f77832cd2881950846db4dd4e0f8e29
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2024-12-07 12:30:18 +0000

    style svg with css instead of js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
 document.getElementById('email-clipboard').addEventListener('click', function () {
     navigator.clipboard.writeText('ethan@hawksley.dev');
 });
-document.getElementById('email-clipboard').addEventListener('mousedown', function () {
-    const svg = document.getElementById('copy-svg');
-    svg.style.fill = '#8f9398';
-});
-window.addEventListener('mouseup', function () {
-    const svg = document.getElementById('copy-svg');
-    svg.style.fill = '#5f6368';
-});
