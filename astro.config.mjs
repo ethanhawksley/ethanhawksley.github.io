20316079f389ac51c12f98eb46192aa1d0a6de21
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2026-02-03 22:13:26 +0000

    Remove astro-compress plugin and related configurations from project

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 import mdx from '@astrojs/mdx';
 
-import compressPlugin from 'astro-compress';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hawksley.dev',
@@ -39,20 +37,5 @@ export default defineConfig({
       lastmod: new Date(),
     }),
     mdx(),
-    compressPlugin({
-      CSS: true,
-      HTML: {
-        'html-minifier-terser': {
-          removeComments: true,
-          collapseWhitespace: true,
-          removeAttributeQuotes: true,
-          minifyCSS: true,
-          minifyJS: true,
-        },
-      },
-      Image: false,
-      JavaScript: true,
-      SVG: true,
-    }),
   ],
 });
