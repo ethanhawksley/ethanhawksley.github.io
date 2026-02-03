7d056d33f27995a4ccbe48c25f251b22547bb22f
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2026-02-03 21:39:26 +0000

    Add RSS feed support and update blog metadata in index.astro

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,24 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import type { APIContext } from 'astro';
+
+export async function GET(context: APIContext) {
+  const posts = await getCollection('posts');
+
+  return rss({
+    title: "Ethan Hawksley's Blog",
+    description:
+      'Articles featuring a variety of topics such as software engineering, computer science concepts, and more!',
+    site: context.site!,
+    items: posts
+      .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
+      .map((post) => ({
+        title: post.data.title,
+        pubDate: post.data.pubDate,
+        description: post.data.description,
+        link: `/blog/${post.id}/`,
+        categories: post.data.tags,
+      })),
+    customData: `<language>en-gb</language>`,
+  });
+}
