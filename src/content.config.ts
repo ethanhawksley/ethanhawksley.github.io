9ca321f4921cb0c31ae11aae847231a4c9cbe2de
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2026-02-06 10:42:17 +0000

    Refactor content loader and improve font preloading in Layout.astro

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { z, defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const posts = defineCollection({
-  loader: glob({ pattern: '**/*.mdx', base: './src/content/blog' }),
+  loader: glob({ pattern: '**/index.mdx', base: './src/content/blog' }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
