44958c985fcb586e3cda03d2548252d3076cc23a
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2026-02-13 17:03:36 +0000

    Enable prefetching in Astro configuration for improved performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hawksley.dev',
+  prefetch: true,
   trailingSlash: 'always',
   image: {
     layout: 'constrained',
