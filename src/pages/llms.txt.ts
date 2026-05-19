db6476ca229726ed5726b00c53af4de42bbb26fd
Ethan Hawksley <noreply@hawksley.dev>
2026-05-19 23:02:30 +0100

    refactor: migrate post fetching to helper function

--- src/pages/llms.txt.ts
+++ src/pages/llms.txt.ts
@@ -1,13 +1,9 @@
-import { getCollection } from 'astro:content';
+import { getSortedPosts, getSortedProjects } from '../lib/content-helpers.ts';
 
 export async function GET() {
-  const posts = (await getCollection('posts')).sort(
-    (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf(),
-  );
+  const allPosts = await getSortedPosts();
 
-  const projects = (await getCollection('projects')).sort(
-    (a, b) => a.data.priority - b.data.priority,
-  );
+  const allProjects = await getSortedProjects();
 
   const content = `# Ethan Hawksley
 
@@ -15,10 +11,10 @@ export async function GET() {
 > Website: https://hawksley.dev
 
 ## Projects
-${projects.map((p) => `- [${p.data.name}](${p.data.liveUrl || p.data.sourceUrl}): ${p.data.description} (${p.data.stack.join(', ')})`).join('\n')}
+${allProjects.map((p) => `- [${p.data.name}](${p.data.liveUrl || p.data.sourceUrl}): ${p.data.description} (${p.data.stack.join(', ')})`).join('\n')}
 
 ## Blog Posts
-${posts.map((p) => `- [${p.data.title}](https://hawksley.dev/blog/${p.id}/): ${p.data.description} (${p.data.pubDate.toISOString().split('T')[0]})`).join('\n')}
+${allPosts.map((p) => `- [${p.data.title}](https://hawksley.dev/blog/${p.id}/): ${p.data.description} (${p.data.pubDate.toISOString().split('T')[0]})`).join('\n')}
 `;
 
   return new Response(content, {
