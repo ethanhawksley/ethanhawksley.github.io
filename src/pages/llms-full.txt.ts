3930b90f06118f362e099b291dfb6f062a6be837
Ethan Hawksley <noreply@hawksley.dev>
2026-05-23 18:57:42 +0100

    feat: create llms-full.txt

---
+++ src/pages/llms-full.txt.ts
@@ -0,0 +1,40 @@
+import { getSortedPosts, getSortedProjects } from '../lib/content-helpers';
+
+export async function GET() {
+  const allPosts = await getSortedPosts();
+
+  const allProjects = await getSortedProjects();
+
+  const content = `# Ethan Hawksley
+
+> Personal site and technical blog of Ethan Hawksley, a UK-based CS student focusing on systems programming, low-level computing, and cybersecurity.
+> Website: https://hawksley.dev
+
+## Projects
+${allProjects
+  .map(
+    (p) => `### ${p.data.name}
+${p.data.liveUrl ? `${p.data.liveUrl} ` : ''}${p.data.sourceUrl}
+${p.data.stack.join(', ')}
+${p.data.description}
+
+`,
+  )
+  .join('')}## Blog Posts
+${allPosts
+  .map(
+    (p) => `
+### ${p.data.title}
+https://hawksley.dev/blog/${p.id}/
+${p.data.description}
+Published ${p.data.pubDate.toISOString().split('T')[0]}${p.data.modDate ? `, Modified ${p.data.modDate.toISOString().split('T')[0]}` : ''}
+${p.body?.replaceAll(/^#/gm, '###')}`,
+  )
+  .join('\n')}`;
+
+  return new Response(content, {
+    headers: {
+      'Content-Type': 'text/plain; charset=utf-8',
+    },
+  });
+}
