6f382cb872b08d02227299a71ba1438aeee41e13
Ethan Hawksley <noreply@hawksley.dev>
2026-06-14 09:30:50 +0100

    refactor: extract profiles to profiles.ts

---
+++ src/utils/profiles.ts
@@ -0,0 +1,106 @@
+export const profiles = [
+  {
+    title: 'Development',
+    links: [
+      {
+        name: 'GitHub',
+        url: 'https://github.com/ethan-hawksley',
+      },
+      {
+        name: 'GitLab',
+        url: 'https://gitlab.com/ethanhawksley',
+      },
+      {
+        name: 'Stack Overflow',
+        url: 'https://stackoverflow.com/users/27157731/ethan-hawksley',
+      },
+      {
+        name: 'LeetCode',
+        url: 'https://leetcode.com/u/ethan-hawksley/',
+      },
+      {
+        name: 'itch.io',
+        url: 'https://ethanhawksley.itch.io',
+      },
+      {
+        name: 'Gravatar',
+        url: 'https://gravatar.com/ethanhawksley',
+      },
+    ],
+  },
+  {
+    title: 'Socials',
+    links: [
+      {
+        name: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/ethan-hawksley',
+      },
+      {
+        name: 'Bluesky',
+        url: 'https://bsky.app/profile/hawksley.dev',
+      },
+      {
+        name: 'Mastodon',
+        url: 'https://mastodon.social/@ethanhawksley',
+      },
+      {
+        name: 'X (Twitter)',
+        url: 'https://x.com/Ethan_Hawksley',
+      },
+      {
+        name: 'YouTube',
+        url: 'https://www.youtube.com/@Ethan-Hawksley',
+      },
+      {
+        name: 'Reddit',
+        url: 'https://www.reddit.com/user/ethanhawksley/',
+      },
+      {
+        name: 'Instagram',
+        url: 'https://www.instagram.com/ethan.hawksley/',
+      },
+    ],
+  },
+  {
+    title: 'Security',
+    links: [
+      {
+        name: 'TryHackMe',
+        url: 'https://tryhackme.com/p/ethanhawksley',
+      },
+      {
+        name: 'Hack The Box',
+        url: 'https://app.hackthebox.com/public/users/3608157',
+      },
+      {
+        name: 'HackerOne',
+        url: 'https://hackerone.com/ethanhawksley',
+      },
+    ],
+  },
+  {
+    title: 'Writing',
+    links: [
+      {
+        name: 'ORCID',
+        url: 'https://orcid.org/0009-0005-6663-0640',
+      },
+      {
+        name: 'DEV Community',
+        url: 'https://dev.to/ethanhawksley',
+      },
+      {
+        name: 'Hashnode',
+        url: 'https://hashnode.com/@ethanhawksley',
+      },
+      {
+        name: 'Medium',
+        url: 'https://medium.com/@ethan-hawksley',
+      },
+      {
+        name: 'Hacker News',
+        url: 'https://news.ycombinator.com/user?id=ethanhawksley',
+      },
+    ],
+  },
+];
