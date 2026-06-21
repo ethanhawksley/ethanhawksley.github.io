c8dafdf7a35b122d66244d74651e153f6ac744e2
Ethan Hawksley <noreply@hawksley.dev>
2026-06-21 19:44:13 +0100

    fix: remove trailing slashes from linkedin

--- src/utils/profiles.ts
+++ src/utils/profiles.ts
@@ -41,7 +41,7 @@ export const profileSections = [
     links: [
       {
         name: 'LinkedIn',
-        url: 'https://www.linkedin.com/in/ethanhawksley/',
+        url: 'https://www.linkedin.com/in/ethanhawksley',
       },
       {
         name: 'Bluesky',
