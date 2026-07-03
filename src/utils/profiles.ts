23e807acadc7c7e9a46c7e99d6262abc90c6cdb8
Ethan Hawksley <noreply@hawksley.dev>
2026-07-03 12:29:24 +0100

    feat: reorganise and add bold.pro

--- src/utils/profiles.ts
+++ src/utils/profiles.ts
@@ -15,14 +15,6 @@ export const profileSections = [
         url: 'https://gitlab.com/ethanhawksley',
       },
       {
-        name: 'Stack Overflow',
-        url: 'https://stackoverflow.com/users/27157731/ethan-hawksley',
-      },
-      {
-        name: 'LeetCode',
-        url: 'https://leetcode.com/u/ethanhawksley/',
-      },
-      {
         name: 'crates.io',
         url: 'https://crates.io/users/ethanhawksley',
       },
@@ -31,18 +23,68 @@ export const profileSections = [
         url: 'https://ethanhawksley.itch.io',
       },
       {
-        name: 'Gravatar',
-        url: 'https://gravatar.com/ethanhawksley',
+        name: 'Stack Overflow',
+        url: 'https://stackoverflow.com/users/27157731/ethan-hawksley',
+      },
+      {
+        name: 'LeetCode',
+        url: 'https://leetcode.com/u/ethanhawksley/',
       },
     ],
   },
   {
-    title: 'Socials',
+    title: 'Professional',
     links: [
       {
         name: 'LinkedIn',
         url: 'https://www.linkedin.com/in/ethanhawksley',
       },
+      {
+        name: 'ORCID',
+        url: 'https://orcid.org/0009-0005-6663-0640',
+      },
+      {
+        name: 'Gravatar',
+        url: 'https://gravatar.com/ethanhawksley',
+      },
+      {
+        name: 'Bold.pro',
+        url: 'https://bold.pro/my/ethanhawksley',
+      },
+    ],
+  },
+  {
+    title: 'Writing',
+    links: [
+      {
+        name: 'DEV Community',
+        url: 'https://dev.to/ethanhawksley',
+      },
+      {
+        name: 'Hashnode',
+        url: 'https://hashnode.com/@ethanhawksley',
+      },
+      {
+        name: 'Medium',
+        url: 'https://medium.com/@ethan-hawksley',
+      },
+      {
+        name: 'Lobsters',
+        url: 'https://lobste.rs/~ethanhawksley',
+      },
+      {
+        name: 'Hacker News',
+        url: 'https://news.ycombinator.com/user?id=ethanhawksley',
+      },
+      {
+        name: 'Hack Club Scrapbook',
+        url: 'https://scrapbook.hackclub.com/ethanhawksley',
+      },
+    ],
+  },
+  {
+    title: 'Social',
+    links: [
       {
         name: 'Bluesky',
         url: 'https://bsky.app/profile/hawksley.dev',
@@ -83,39 +125,6 @@ export const profileSections = [
       {
         name: 'HackerOne',
         url: 'https://hackerone.com/ethanhawksley',
-      },
-    ],
-  },
-  {
-    title: 'Writing',
-    links: [
-      {
-        name: 'ORCID',
-        url: 'https://orcid.org/0009-0005-6663-0640',
-      },
-      {
-        name: 'DEV Community',
-        url: 'https://dev.to/ethanhawksley',
-      },
-      {
-        name: 'Hashnode',
-        url: 'https://hashnode.com/@ethanhawksley',
-      },
-      {
-        name: 'Medium',
-        url: 'https://medium.com/@ethan-hawksley',
-      },
-      {
-        name: 'Lobsters',
-        url: 'https://lobste.rs/~ethanhawksley',
-      },
-      {
-        name: 'Hacker News',
-        url: 'https://news.ycombinator.com/user?id=ethanhawksley',
-      },
-      {
-        name: 'Hack Club Scrapbook',
-        url: 'https://scrapbook.hackclub.com/ethanhawksley',
       },
     ],
   },
