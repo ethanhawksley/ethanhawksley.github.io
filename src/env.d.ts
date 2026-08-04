e767479ba2c930b56e1d8c0317061712ef7d8f60
Ethan Hawksley <noreply@hawksley.dev>
2026-08-04 18:32:20 +0100

    feat: add easter egg

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,4 +7,6 @@ interface Navigator {
 interface Window {
   __getTheme: () => 'light' | 'dark';
   __setTheme: (theme: 'light' | 'dark', save?: boolean, doc?: Document) => void;
+  Yes: any;
+  No: any;
 }
