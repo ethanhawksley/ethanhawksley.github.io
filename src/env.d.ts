5b802331d13a5b2adcdd00b26e6b665868a6b5c7
Ethan Hawksley <noreply@hawksley.dev>
2026-09-04 13:23:55 +0100

    feat: use view transitions for theme toggle

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,6 +7,14 @@ interface Navigator {
 interface Window {
   __getTheme: () => 'light' | 'dark';
   __setTheme: (theme: 'light' | 'dark', save?: boolean, doc?: Document) => void;
+  __themeTransition?:
+    | ((updateCallback: () => Promise<void> | void) => {
+        ready: Promise<void>;
+        finished: Promise<void>;
+        updateCallbackDone: Promise<void>;
+        skipTransition: () => void;
+      })
+    | null;
   yes: any;
   no: any;
 }
